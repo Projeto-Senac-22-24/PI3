271c580a9ac81f632c851671cedeb755aa299f33
rodrigo Junior <77363230+rodrigojunior07@users.noreply.github.com>
2023-06-01 15:15:22 -0300

    Update InterProdutoDao.java
    Signed-off-by: rodrigo Junior <77363230+rodrigojunior07@users.noreply.github.com>

--- src/main/java/br/com/SuplaMentePI/servlet/dao/InterProdutoDao.java
+++ src/main/java/br/com/SuplaMentePI/servlet/dao/InterProdutoDao.java
@@ -1,6 +1,7 @@
 package br.com.SuplaMentePI.servlet.dao;
 
 
+
 import br.com.SuplaMentePI.servlet.modelos.Categorias;
 import br.com.SuplaMentePI.servlet.modelos.Produto;
 
@@ -10,8 +11,17 @@ import java.util.Optional;
 public interface InterProdutoDao {
     Produto save(Produto produto);
     Produto update(Produto produto);
-    void deleta(long id);List<Produto> findAll();// ira devolver todos os produtos
+    void deleta(long id);
+
+    void deleta(Long id);
+
+    List<Produto> findAll();// ira devolver todos os produtos
     Optional<Produto> findById(long id);// ira buscar um produto pelo id
    List<Produto> findByString(String Nome);
+
+    Optional<Produto> findById(Long id);
+
+    List<Produto> findByNome(String nome);
+
     List<Produto> findByCategoria(Categorias categoria);
 }
